30cc26c3e79f80b9f15bf544f83ccc9b337ef8bc
Mario Alberto Ibarra Manzano <ibarram@ugto.mx>
2024-09-23 18:54:14 -0600

    Update 014

--- src/014_Calidad.c
+++ src/014_Calidad.c
@@ -9,14 +9,23 @@
 int main(int argc, char*argv[])
 {
 	float P[N];
-	int C[N], E[NE], i;
+	int C[N], E[NE][NE], i, j, k, aux, flag;
 	srand(time(NULL));
 	for(i=0; i<N; i++)
 		P[i] = (100.0*rand())/RAND_MAX;
 	for(i=0; i<N; i++)
 		C[i] = (P[i]<(100-NC)?0:1);
 	for(i=0; i<NE; i++)
-		E[i] = i*(N/NE)+rand()%(N/NE);
+	{
+		E[i][0] = i*(N/NE)+rand()%(N/NE);
+		j = 1;
+		flag = 0;
+		do{
+			aux = i*(N/NE)+rand()%(N/NE);
+			for(k=0; k<j-1; k++)
+		}while(flag);
+		E[i][j] = aux;
+	}
 	for(i=0; i<NE; i++)
 		printf("%d. %d - %d\n", i+1, C[E[i]], E[i]);
 	return 0;
