1a8fc7bcf7b18fb84d796816d579fe3e3a5fbba7
Mario Alberto Ibarra Manzano <ibarram@ugto.mx>
2025-03-20 13:45:25 -0600

    Update

--- src/2025_1/017_DDU.c
+++ src/2025_1/017_DDU.c
@@ -3,10 +3,31 @@
 #include<math.h>
 #include<time.h>
 
+long int suma(long int x[], int n)
+{
+	return n?x[0]+suma(x+1,n-1):0;
+}
+
+long int suma2(long int x[], int n)
+{
+	return n?x[0]*x[0]+suma2(x+1,n-1):0;
+}
+
+float media(long int x[], long int n)
+{
+	return ((float)suma(x, n))/n;
+}
+
+float varianza(long int x[], long int n)
+{
+	return ((float)suma2(x, n))/n-media(x, n)*media(x, n);
+}
+
 int main(int argc, char *argv[])
 {
-	long int a, b, n, i, j, m, *x, *y, rnd;
+	long int a, b, n, i, j, m, *x, *y, *dx, rnd, ea, eb;
 	int fm, flag;
+	float mx, s2x;
 	printf("Ingrese el valor minimo: ");
 	scanf("%ld", &a);
 	printf("Ingrese el valor maximo: ");
@@ -34,6 +55,13 @@ int main(int argc, char *argv[])
 		free(x);
 		return 2;
 	}
+	dx = malloc(m*sizeof(long int));
+	if(dx==NULL)
+	{
+		free(x);
+		free(y);
+		return 3;
+	}
 	srand(time(NULL));
 	for(i=0; i<m; i++)
 	{
@@ -49,11 +77,20 @@ int main(int argc, char *argv[])
 
 		}while(flag);
 		y[i] = rnd;
+		dx[i] = x[y[i]];
 	}
 	printf("a = %ld\tb = %ld\tn = %ld\n", a, b, n);
 	for(i=0; i<m; i++)
-		printf("x[%ld] = %ld\n", y[i], x[y[i]]);
+		printf("x[%ld] = %ld\n", y[i], dx[i]);
+	mx = media(dx, m);
+	printf("Media = %f\n", mx);
+	s2x = varianza(dx, m);
+	printf("Varianza = %f\n", s2x);
+	ea = floor(mx+.5-sqrt(12*s2x+1)/2);
+	eb = ceil(mx-.5+sqrt(12*s2x+1)/2);
+	printf("a = %ld\tb = %ld\tm=%ld\n", ea, eb, m);
 	free(y);
 	free(x);
+	free(dx);
 	return 0;
 }
